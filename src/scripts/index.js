cc7499b95be8c6dbbf447c23a30151e8c31d2ffc
Julian George <jules104@gmail.com>
2019-12-12 10:04:07 +0000

    Destructuring objects example

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,6 @@
-console.log("hello world!¡¡¡¡¡");
+let car = { id: 5000, style: "convertible " };
+let id, style;
+({ id, style } = car);
+// Curly braces are used for code blocks and destructuring.
+// So use brackets to wrap destructuring statement so compiler knows its for destructuring.
+console.log(id, style); // logs out 5000 convertible
