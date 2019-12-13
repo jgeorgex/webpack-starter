187f6c750b39525d3392f261fb173b8e34f589a4
Julian George <jules104@gmail.com>
2019-12-13 09:55:04 +0000

    Example of block IIFE

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,9 @@
-let message = "Outside";
-if (5 === 5) {
-  let message = "Equal";
-  console.log(message);
-}
-console.log(message);
+//IIFE function.  Encapsulated function followed by () to imediately invoke it.
+(function() {
+  console.log("in function");
+})();
+
+//regular function - wont execute.  Doesnt have a name and nothing is being done to call it.
+// function() {
+//     console.log('in function');
+// }
