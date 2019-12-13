6ad391508300219e5d1bbe32c4a34ef0440333d0
Julian George <jules104@gmail.com>
2019-12-13 11:16:52 +0000

    Importing a module example

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,4 @@
-//IIFE function.  Encapsulated function followed by () to imediately invoke it.
-(function() {
-  console.log("in function");
-})();
+import { Car } from "./models/car.js";
 
-//regular function - wont execute.  Doesnt have a name and nothing is being done to call it.
-// function() {
-//     console.log('in function');
-// }
+let car = new Car(123);
+console.log(car.id);
